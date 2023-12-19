a1f2b16ca72da2abdd512c36160ca12f9d2d353c
Milan <142885367+bettercallmilan@users.noreply.github.com>
2023-12-19 09:42:11 +0100

    Add files via upload

---
+++ Taschenrechner1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel.Design;
+
+namespace Taschenrechner1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    double zahl1;
+                    double zahl2;
+                    char operation;
+                    double ergebnis = 0;
+
+                    Console.Write("Gib die 1. Zahl ein: ");
+                    zahl1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Gib die 2. Zahl ein: ");
+                    zahl2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Gib die Operation ein (+/-/*/÷): ");
+                    operation = Convert.ToChar(Console.ReadLine());
+
+                    switch (operation)
+                    {
+                        case '+':
+                            ergebnis = zahl1 + zahl2;
+                            break;
+                        case '-':
+                            ergebnis = zahl1 - zahl2;
+                            break;
+                        case '*':
+                            ergebnis = zahl1 * zahl2;
+                            break;
+                        case '÷':
+                            if (zahl2 != 0)
+                            {
+                                ergebnis = zahl1 / zahl2;
+                            } else
+                            {
+                                Console.WriteLine("Man kann durch 0 nicht teilen.");
+                            }
+                            break;
+                        case '/':
+                            if (zahl2 != 0)
+                            {
+                                ergebnis = zahl1 / zahl2;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Man kann durch 0 nicht teilen.");
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Ungültige Operation.");
+                            break;
+                    }
+
+                    Console.WriteLine($"Ergebnis: {ergebnis}");
+                    Console.WriteLine("------------------------------");
+                }
+                catch
+                {
+                    Console.WriteLine("Ein Fehler ist aufgetreten.");
+                }
+            }
+        }
+    }
+}
