8b00ab3de9f711bdc8569c327c46c71974c4c9b9
Milan <142885367+bettercallmilan@users.noreply.github.com>
2024-01-23 13:56:11 +0100

    Add files via upload

--- Kalender/Program.cs
+++ Kalender/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
 
 namespace Kalender
 {
@@ -21,6 +22,7 @@ namespace Kalender
             Console.WriteLine("Termine abrufen: R");
             Console.WriteLine("Termin löschen: D");
             Console.WriteLine("Termin ändern: A");
+            Console.WriteLine("");
 
             while (true)
             {
@@ -41,31 +43,95 @@ namespace Kalender
 
                                 termine[date] = termin;
                                 Console.WriteLine($"Termin am {date.ToShortDateString()} eingetragen: {termin}");
+                                Console.WriteLine("");
                             }
                             else
                             {
                                 Console.WriteLine("Ungültiges Datumsformat. Bitte verwenden Sie das Format JJJJ-MM-TT.");
+                                Console.WriteLine("");
                             }
                             break;
 
                         case ConsoleKey.R:
-                            foreach (var item in termine)
+                            if (termine.Count > 0)
                             {
-                                Console.WriteLine(item);
+                                foreach (var item in termine)
+                                {
+                                    Console.WriteLine(item);
+                                    Console.WriteLine("");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Keine Termine gefunden.");
+                                Console.WriteLine("");
                             }
                             break;
 
                         case ConsoleKey.A:
-                            Console.WriteLine("-");
+                            Console.WriteLine("Geben Sie den gewünschten Datum ein, den Sie verändern möchten (im Format JJJJ-MM-TT):");
+                            string datumAendern = Console.ReadLine();
+
+                            if (DateTime.TryParse(datumAendern, out DateTime datumAendern1) && termine.ContainsKey(datumAendern1))
+                            {
+                                Console.WriteLine($"Aktueller Termin für {datumAendern1.ToShortDateString()}: {termine[datumAendern1]}");
+                                Console.WriteLine("Geben Sie den neuen Termin ein:");
+                                string neuerTermin = Console.ReadLine();
+
+                                termine[datumAendern1] = neuerTermin;
+                                Console.WriteLine($"Termin am {datumAendern1.ToShortDateString()} aktualisiert: {neuerTermin}");
+                                Console.WriteLine("");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ungültiges Datum oder kein Termin für das angegebene Datum gefunden.");
+                                Console.WriteLine("");
+                            }
                             break;
 
                         case ConsoleKey.D:
-                            Console.WriteLine("-");
+                            Console.WriteLine("Geben Sie den gewünschten Datum ein, den Sie löschen möchten (im Format JJJJ-MM-TT):");
+                            string datumLoeschen = Console.ReadLine();
+
+                            if (DateTime.TryParse(datumLoeschen, out DateTime datumLoeschen1) && termine.ContainsKey(datumLoeschen1))
+                            {
+                                Console.WriteLine($"Aktueller Termin für {datumLoeschen1.ToShortDateString()}: {termine[datumLoeschen1]}");
+                                Console.WriteLine("Sind Sie sicher, dass Sie diesen Termin löschen möchten ('Y' = Ja, 'N' = Nein)?");
+
+                                ConsoleKeyInfo taste1 = Console.ReadKey(true);
+
+                                switch (taste1.Key)
+                                {
+                                    case ConsoleKey.Y:
+                                        termine.Remove(datumLoeschen1);
+                                        Console.WriteLine($"Termin am {datumLoeschen1} erfolgreich gelöscht.");
+                                        Console.WriteLine("");
+                                        break;
+
+                                    case ConsoleKey.N:
+                                        Console.WriteLine("Terminlöschung abgebrochen.");
+                                        Console.WriteLine("");
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("Ungültige Taste. Drücken Sie 'Y' (Termin löschen) oder 'N' (Termin beibehalten).");
+                                        Console.WriteLine("");
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ungültiges Datum oder kein Termin für das angegebene Datum gefunden.");
+                                Console.WriteLine("");
+
+                            }
                             break;
 
                         default:
                             Console.WriteLine("Ungültige Taste. Drücken Sie 'N' (neues Termin), 'R' (Termine abrufen), 'D' (Termin löschen) oder 'A' (Termin verändern).");
+
                             break;
+
                     }
                 }
             }
