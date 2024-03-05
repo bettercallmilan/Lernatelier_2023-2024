65340d625edc2076abe154c1e5828fd7598a58db
Milan <142885367+bettercallmilan@users.noreply.github.com>
2024-03-05 11:09:44 +0100

    Add files via upload

---
+++ Kalender_Forms1/Program.cs
@@ -0,0 +1,17 @@
+namespace Kalender_Forms1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Kalender());
+        }
+    }
+}
